b9545c4a9c408bce347e7c80edebdf6d038cd7bb
GillianE <geigo@aclu.org>
2018-09-12 16:30:05 -0400

    take out all records

--- FacebookAdsTargeting.sql
+++ FacebookAdsTargeting.sql
@@ -87,20 +87,6 @@ select e.email_address__c as email
    and e.undeliverable__c = 'false'
    and a.rc_giving__active_sustainer__c = 'true';
 
-   --All Records
-select e.email_address__c as email
-     , c.Phone as phone
-     , c.firstname as fn
-     , c.lastname as ln
-     , c.mailingcity as ct 
-     , c.mailingstate as st
-     , c.mailingpostalcode as zip
-     , c.mailingcountry as country
-     , c.birthdate as dob
-  from sf.contact c
-  join sf.email e on c.Id = e.contact__c
- where e.opt_in__c = 'true'
-   and e.undeliverable__c = 'false';
   
 
 
