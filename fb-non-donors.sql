8b68fe5267706a7cddc73f1ee0791cbaf641ca51
GillianE <geigo@aclu.org>
2018-09-12 13:44:46 -0400

    added fb col names

--- fb-non-donors.sql
+++ fb-non-donors.sql
@@ -1,12 +1,12 @@
-select e.email_address__c
-     , c.Phone
-     , c.firstname
-     , c.lastname
-     , c.mailingcity
-     , c.mailingstate
-     , c.mailingpostalcode
-     , c.mailingcountry
-     , c.birthdate
+select e.email_address__c as email
+     , c.Phone as phone
+     , c.firstname as fn
+     , c.lastname as ln
+     , c.mailingcity as ct 
+     , c.mailingstate as st
+     , c.mailingpostalcode as zip
+     , c.mailingcountry as country
+     , c.birthdate as dob
   from sf.contact c
   join sf.email e on c.Id = e.contact__c
  where e.opt_in__c = 'true'
